0cd8c885d8b6c7a01cd9a83eedd0dcd08ccc7d12
Daniel Morady <daniel-morady@web.de>
2024-03-13 12:52:35 +0100

    fixing docker filter for search

--- heal.go
+++ heal.go
@@ -14,15 +14,23 @@ const (
 	dockerSock   = "DOCKER_SOCK"
 	healStart    = "HEAL_START_PERIOD"
 	healInterval = "HEAL_INTERVAL"
+	healLabel    = "HEAL_LABEL"
 )
 
 type container struct {
 	Id string
 }
 
-func restartContainers(url string) {
+type containerFilter struct {
+	Health []string `json:"health"`
+	Label  []string `json:"label"`
+	Status []string `json:"status"`
+}
+
+func restartContainers(url string, containerFilter string) {
 	containerUrl := fmt.Sprintf("%s/v1.44/containers", url)
-	resp, err := http.Get(containerUrl + "/json?health=unhealthy")
+
+	resp, err := http.Get(fmt.Sprintf("%s/json?filters=%s", containerUrl, containerFilter))
 	if err != nil {
 		log.Fatalf("http:get %s", err)
 	}
@@ -65,13 +73,27 @@ func main() {
 	dockerSocket := "http://localhost:3001"
 	interval := getEnvDuration(5.0*(time.Second), healInterval)
 	startPeriod := getEnvDuration(time.Duration(0), healStart)
+	label := "health"
+	x := containerFilter{
+		Health: []string{"unhealthy"},
+		Label:  []string{label},
+		Status: []string{"running"},
+	}
+	j, _ := json.Marshal(x)
+	print(string(j))
 	val, ok := os.LookupEnv(dockerSock)
 	if ok {
 		dockerSocket = val
 	}
+
+	val, ok = os.LookupEnv(healLabel)
+	if ok {
+		label = val
+	}
+
 	time.Sleep(startPeriod)
 	for {
-		restartContainers(dockerSocket)
+		restartContainers(dockerSocket, label)
 		time.Sleep(interval)
 	}
 }
